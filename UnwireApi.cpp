621ebc188410e8915d0ef9435013a9611522c30c
Justin LeMay <justin@justinlemay.com>
2021-02-04 22:17:57 -0800

    got it to work

--- UnwireApi.cpp
+++ UnwireApi.cpp
@@ -1,18 +1,26 @@
-// unwire-api.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// unwire-api.cpp : 
 
 #include <iostream>
-#include "curl\curl.h"
+#include <fstream>
+#include <stdio.h>
+#include "curl/curl.h"
 #include <string>
 #include <nlohmann/json.hpp>
 #include <vector>
-#include <UnwireApi.h>
 
 using json = nlohmann::json;
 
 using std::string;
 using std::cout;
 using std::vector;
+
+#ifdef __linux__ 
+    //#TODO: implement a better function
+#elif _WIN32
+    // windows code goes here
+#else
+
+#endif
 
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
 {
@@ -26,7 +34,9 @@ static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* use
 char* stringToCharP(string str0) {
     int n = str0.length();
     char* char_p = new char[n + 1];
-    strcpy_s(char_p, n + 1, str0.c_str());
+    strcpy(char_p, str0.c_str());
+    //#TODO: decide what to do with this for linux
+    //strcpy_s(char_p, n + 1, str0.c_str());
     return char_p;
 }
 
@@ -127,10 +137,11 @@ AuthToken initialLogin(string email, string password) {
 
 int main()
 {
-    std::ifstream file("file.json");
+    std::ifstream file("creds.json");
     json j;
     file >> j;
-    AuthToken authToken = initialLogin(j["email", j["password"]);
+    cout << "got here";
+    AuthToken authToken = initialLogin(j["email"], j["password"]);
 
     cout << "Login successfull. Access token: " + authToken.accessToken + ", UUID: " + authToken.userUUID << std::endl;
     return 0;
@@ -152,13 +163,3 @@ int main()
     return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
